5c600213be11b6ea28e037d862fd6ed98ead7a6f
Shivangkumar Gandhi <shivanggandhi19@gmail.com>
2021-02-14 12:59:12 +0530

    Dashboard Part 1 Completed

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,3 +1,4 @@
+import { DashboardModule } from './../pages/dashboard/dashboard.module';
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -6,13 +7,20 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+import { AccountModule } from '../pages/account/account.module';
+import { ActivityModule } from '../pages/activity/activity.module';
+import { BudgetModule } from '../pages/budget/budget.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    DashboardModule,
+    AccountModule,
+    ActivityModule,
+    BudgetModule
   ],
   declarations: [TabsPage]
 })
